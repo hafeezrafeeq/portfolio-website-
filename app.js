e23bb09e0beb7682d99e700d9f622ecf48c9adf3
hafeezrafeeqgit config --global user.name hafeezrafeeq <hafeezrafeeq031@gmail.com>
2025-09-16 00:28:05 +0500

    Refactor GSAP timeline for smoother animations; update Locomotive Scroll integration and enhance animation timing

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-// const  tl =  gsap.timeline()
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#page1'),
+    smooth: true
+});
 
-// tl.to("#page1", {
-//     y: "100vh",
-//     scale:0.5,
-//     duration:0,
+const  tl =  gsap.timeline()
 
-// })
+tl.to("#page1", {
+    y: "100vh",
+    scale:0.5,
+    duration:0,
 
-// tl.to("#page1", {
-//     y: "0vh",
-//     duration:1,
+})
 
-// })
+tl.to("#page1", {
+    y: "0vh",
+    duration:1,
 
-// tl.to("#page1", {
-//     rotate: 360,
-//     scale:1,
-//     duration:1,  
-// })
+})
+
+tl.to("#page1", {
+    rotate: 360,
+    scale:1,
+    duration:1,  
+})
 
 const cards = document.querySelectorAll(".card");
 
